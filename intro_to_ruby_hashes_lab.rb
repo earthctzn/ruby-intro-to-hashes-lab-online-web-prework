87fcca91232906ebc351e5074d512a3227cd6d16
Caleb Einarr <earthctzn1@gmail.com>
2019-04-09 23:55:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,14 +14,14 @@ def monopoly
 end
 
 def monopoly_with_second_tier
- :new_hash => {
+ :new_hash = {
    :railroads => {
      :pieces => 4, 
      :rent_in_dollars => {}}
 end
 
 def monopoly_with_third_tier
- :new_hash => {
+ :new_hash = {
    :railroads => {
    :pieces => 4, 
    :rent_in_dollars => {
@@ -39,7 +39,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
- :new_hash => {
+ :new_hash = {
    :railroads => {
      :pieces => 4, 
      :rent_in_dollars => {
