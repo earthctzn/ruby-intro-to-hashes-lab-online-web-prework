6be3413887b1e242d44ed549effcab7a4b45ccc3
Caleb Einarr <earthctzn1@gmail.com>
2019-04-09 23:19:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- :railroads[:names] = {:reading_railroad=>{}, :pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline=>{},:pieces=>{}, :rent_in_dollars=>{
+ :railroads[{:names = {:reading_railroad=>{},
+ :pennsylvania_railroad=>{}, 
+ :b_and_o_railroad=>{}, 
+ :shortline=>{},
+ :rent_in_dollars=>{
       :one_piece_owned=> 25,
       :two_pieces_owned=> 50,
       :three_pieces_owned=> 100,
@@ -30,5 +34,6 @@ def monopoly_with_fourth_tier
 end
 
 
-
+:hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age =>  "18", :attitude => "hot-headed"}
+      ]
 
