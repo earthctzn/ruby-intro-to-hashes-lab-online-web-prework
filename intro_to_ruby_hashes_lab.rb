3928832a860401b242940c139930ae9855d6a096
Caleb Einarr <earthctzn1@gmail.com>
2019-04-09 23:20:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- :railroads[{:names = {:reading_railroad=>{},
+ :railroads=>[{:names = {:reading_railroad=>{},
  :pennsylvania_railroad=>{}, 
  :b_and_o_railroad=>{}, 
  :shortline=>{},
