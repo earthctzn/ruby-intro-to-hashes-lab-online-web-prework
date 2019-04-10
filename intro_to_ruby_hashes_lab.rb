6eac28d411e7a630e4e9bb33be80f12a04c06615
Caleb Einarr <earthctzn1@gmail.com>
2019-04-10 00:12:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,15 +31,16 @@ def monopoly_with_third_tier
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
-        :four_pieces_owned => 200}
+        :four_pieces_owned => 200
+        }
     },
       :names => {:reading_railroad => {},
       :pennsylvania_railroad => {}, 
       :b_and_o_railroad => {}, 
       :shortline => {}
-    }
+      }
    }
-end
+ end
 
 def monopoly_with_fourth_tier
    new_hash = {
@@ -49,14 +50,14 @@ def monopoly_with_fourth_tier
           :one_piece_owned => 25,
           :two_pieces_owned => 50,
           :three_pieces_owned => 100,
-          :four_pieces_owned => 200}
+          :four_pieces_owned => 200
+          }
       },
         :names => {
           :reading_railroad => {"mortgage_value"=>"$100"}, 
           :pennsylvania_railroad => {"mortgage_value"=>"$200"}, 
           :b_and_o_railroad => {"mortgage_value"=>"$400"}, 
           :shortline => {"mortgage_value"=>"$800"}
-      }
+          }
     }
-    end
 end
