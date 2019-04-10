b3267648b8088da25b0632356a8df30ce5ab66c3
Caleb Einarr <earthctzn1@gmail.com>
2019-04-10 00:29:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,9 @@ def monopoly_with_second_tier
 
 def monopoly_with_third_tier
    new_hash = {
-     :railroads => {
-     :pieces => 4, 
+    :railroads => {
+     :pieces => 4,
+     :names => {},
      :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
